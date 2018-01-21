8a125bf70033e73207d71e7677817848510e77b4
Govind <imgovind@live.com>
2018-01-21 14:31:07 -0800

    Added all three object rotations

--- worldTracking/worldTracking/ViewController.swift
+++ worldTracking/worldTracking/ViewController.swift
@@ -26,17 +26,63 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func add(_ sender: Any) {
-//        self.addNodeToParent(node: getShape())
-//        self.addNodeToParent(node: getHouseShape())
-        self.addMultipleRelativeNodes()
-    }
     
     @IBAction func reset(_ sender: Any) {
         self.restartSession()
     }
     
+    @IBAction func add(_ sender: Any) {
+//        self.addNodeToParent(node: getShape())
+//        self.addNodeToParent(node: getHouseShape())
+//        self.addMultipleRelativeNodes()
+        self.addRotatedNodes()
+    }
+    
+    func addRotatedNodes() {
+        // horizontal rotation
+        // y-axis rotation
+        let box = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.0))
+        box.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        box.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        box.eulerAngles = SCNVector3(0, Float(45.degreeToRadians), 0)
+        box.position = SCNVector3(-0.2, 0.0, -0.2)
+        let ball = SCNNode(geometry: SCNSphere(radius: 0.05))
+        ball.geometry?.firstMaterial?.specular.contents = UIColor.green
+        ball.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        ball.position = SCNVector3(0.0, 0.0, -0.3)
+        box.addChildNode(ball)
+        self.sceneView.scene.rootNode.addChildNode(box)
+        
+        // rotation
+        // z-axis rotation
+        let cylinder = SCNNode(geometry: SCNCylinder(radius: 0.05, height: 0.1))
+        cylinder.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        cylinder.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        cylinder.position = SCNVector3(0.0, 0.0, -0.2)
+        cylinder.eulerAngles = SCNVector3(0, 0, Float(45.degreeToRadians))
+        let pyramid = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
+        pyramid.geometry?.firstMaterial?.specular.contents = UIColor.green
+        pyramid.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        pyramid.position = SCNVector3(0.0, 0.0, -0.3)
+        cylinder.addChildNode(pyramid)
+        self.sceneView.scene.rootNode.addChildNode(cylinder)
+        
+        // vertical rotation
+        // x-axis rotation
+        let plane = SCNNode(geometry: SCNPlane(width: 0.1, height: 0.1))
+        plane.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        plane.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        plane.eulerAngles = SCNVector3(Float(45.degreeToRadians), 0, 0)
+        plane.position = SCNVector3(0.2, 0.0, -0.2)
+        let capsule = SCNNode(geometry: SCNCapsule(capRadius: 0.025, height: 0.1))
+        capsule.geometry?.firstMaterial?.specular.contents = UIColor.green
+        capsule.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        capsule.position = SCNVector3(0.0, 0.0, -0.3)
+        plane.addChildNode(capsule)
+        
+        self.sceneView.scene.rootNode.addChildNode(plane)
+    }
+
     func addMultipleRelativeNodes() {
         let pyramid = getShape(inputNumber: 4)
         pyramid.geometry?.firstMaterial?.specular.contents = UIColor.orange
@@ -148,5 +194,9 @@ class ViewController: UIViewController {
     func randomGenerator(start: CGFloat, end: CGFloat) -> CGFloat {
         return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(start-end) + min(start, end)
     }
+}
+
+extension Int {
+    var degreeToRadians: Double { return Double(self) * .pi/180 }
 }
 
